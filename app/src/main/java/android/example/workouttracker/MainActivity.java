8a8f6ade2122d4b556035d5fd834138ae7ad23b0
Kei Nakano <keinakano98@gmail.com>
2020-03-18 16:53:39 -0700

    Main Menu Buttons Added in XML

--- app/src/main/java/android/example/workouttracker/MainActivity.java
+++ app/src/main/java/android/example/workouttracker/MainActivity.java
@@ -3,6 +3,10 @@ package android.example.workouttracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import static java.sql.DriverManager.println;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void execute(View view){
+        //Open new activity here
+       Log.v("debug","EXECUTE");
+
+    }
+
+    public void selectEdit(View view){
+        Log.v("debug","SlEDIT");
+    }
+
+    public void stats(View view){
+        Log.v("debug","STATS");
+    }
+
+    public void history(View view){
+        Log.v("debug","HISTORY");
     }
 }
